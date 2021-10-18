f9fc813b3bd62ec2b13060336b20c9e38fde249e
MIcah Morgan <micah.m239@stu.nebo.edu>
2021-10-18 09:12:36 -0600

    Bullet code

--- First person game2/Assets/Scripts/PlayerController.cs
+++ First person game2/Assets/Scripts/PlayerController.cs
@@ -6,62 +6,54 @@ public class PlayerController : MonoBehaviour
 {
     // Movement
     public float moveSpeed; // How fast the player moves
-    public float jumpForce;
+
+    public float jumpForce; // How High The player jumps
     //Camera
     public float lookSensitivity; // Mouse movement sensitivity
+
     public float maxLookX; // Lowest down we can look
+
     public float minLookX; // Highest up we can look
+    
     private float rotX;    // Current x rotation of the camera
 
     //Components
     private Camera cam;
+
     private Rigidbody rb;
+
+    private Weapon weapon;
 
     void Awake()
     {
         // Get the components
         cam = Camera.main;
         rb = GetComponent<Rigidbody>();
+
+        weapon = GetComponent<Weapon>();
     }
     // Update is called once per frame
     void Update()
     {
         Move();
         CamLook();
+        // Jump Button
         if(Input.GetButtonDown("Jump"))
             Jump();
+            // Fire Button
+            if(Input.GetButton("Fire1"))
+            {
+                if(weapon.CanShoot())
+                    weapon.Shoot();
+            }
     }
     void Move()
-    {   // Get Keyboard input with move speed
+   {  // Get Keyboard input with move speed
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
         // Applying movement to the rigidbody
         Vector3 dir = transform.right * x + transform.forward * z;
-        // Jump direction
-        dir.y = rb.velocity.y;
-        // Apply direction to camera movement
-        rb.velocity = dir;
-    }
-    void Jump()
-    {
-        // Cast ray to ground
-        Ray ray = new Ray(transform.position, Vector3.down);
-        // Check Ray length to jump
-        if(Physics.Raycast(ray, 1.1f))
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    }
-    void CamLook()
-    {
-        // Get mouse input so we can look around with the mouse
-        float y = Input.GetAxis("Mouse X") * lookSensitivity;
-        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
-
-        // Clamp the vertical rotation of the camera
-        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
-
-        // Applying the rotation to the camera
-        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
-        transform.eulerAngles += Vector3.up * y;
-    }
+   }
+    
 
 }
---
+++ First person game2/Assets/Scripts/Weapon.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletProjectile;
+    public Transform muzzle;
+    public int curAmmo;
+    public int maxAmmo;
+    public bool infiniteAmmo;
+    public float bulletSpeed;
+    public float shootrate;
+    public float lastShootTime;
+    private bool isPlayer
+
+    void Awake ()
+
+    {
+        // are we attached to the player 
+        if(GetComponent<PlayerController>())
+        {
+        isPlayer = true;
+        }
+
+    }
+    // can we shooot a bullet
+    public bool CanShoot()
+    {
+        if(Time.Time - lastShootTime> = shootRate)
+        {
+            if(curAmmo > 0 || infiniteAmmo == true)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void Shoot()
+    {
+        lastShootTime = Time.time;
+        curAmmo --;
+
+        GameObject bullet = Instantiate(bulletProjectile, muxxle.position, muzzle.rotation)
+
+        // Set Velocity of bulletProjectile
+        bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
