74dd1af5d28ffd936b1ce40681118e29edeac176
MIcah Morgan <micah.m239@stu.nebo.edu>
2021-10-25 09:07:08 -0600

    built base

--- First person game2/Assets/Scripts/PlayerController.cs
+++ First person game2/Assets/Scripts/PlayerController.cs
@@ -6,25 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     // Movement
     public float moveSpeed; // How fast the player moves
-
-    public float jumpForce; // How High The player jumps
-    //Camera
+    public float jumpForce; // How high the player jumps
+    // Camera
     public float lookSensitivity; // Mouse movement sensitivity
-
-    public float maxLookX; // Lowest down we can look
-
-    public float minLookX; // Highest up we can look
-    
-    private float rotX;    // Current x rotation of the camera
-
+    public float maxLookX;  // Lowest down we can look
+    public float minLookX;  // Highest up we can look
+    private float rotX;     // Current x rotation of the camera
     //Components
     private Camera cam;
-
     private Rigidbody rb;
 
     private Weapon weapon;
 
-    void Awake()
+    void Awake() 
     {
         // Get the components
         cam = Camera.main;
@@ -32,6 +26,7 @@ public class PlayerController : MonoBehaviour
 
         weapon = GetComponent<Weapon>();
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -40,20 +35,46 @@ public class PlayerController : MonoBehaviour
         // Jump Button
         if(Input.GetButtonDown("Jump"))
             Jump();
-            // Fire Button
-            if(Input.GetButton("Fire1"))
-            {
-                if(weapon.CanShoot())
-                    weapon.Shoot();
-            }
+        // Fire Button
+        if(Input.GetButton("Fire1"))
+        {
+            if(weapon.CanShoot())
+                weapon.Shoot();
+        }    
     }
+
     void Move()
-   {  // Get Keyboard input with move speed
+    {   // Get Keyboard input with move speed 
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
         // Applying movement to the rigidbody
         Vector3 dir = transform.right * x + transform.forward * z;
-   }
-    
+        // Jump direction
+        dir.y = rb.velocity.y;
+        // Apply directio to camera movement
+        rb.velocity = dir;
+    }
 
+    void Jump()
+    {
+        // Cast ray to ground
+        Ray ray = new Ray(transform.position, Vector3.down);
+        // Check Ray length to jump
+        if(Physics.Raycast(ray, 1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void CamLook()
+    {
+        // Get mouse input so we can look around with the mouse
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        // Clamp the vertical rotation of the camera
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+
+        // Applying the rotation to Camera
+        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+    }
 }
--- First person game2/Assets/Scripts/Weapon.cs
+++ First person game2/Assets/Scripts/Weapon.cs
@@ -6,30 +6,32 @@ public class Weapon : MonoBehaviour
 {
     public GameObject bulletProjectile;
     public Transform muzzle;
+
     public int curAmmo;
     public int maxAmmo;
     public bool infiniteAmmo;
+
     public float bulletSpeed;
-    public float shootrate;
+
+    public float shootRate;
     public float lastShootTime;
     private bool isPlayer;
 
-    void Awake ()
 
+    void Awake ()
     {
-        // are we attached to the player 
+        // Are we attached to the player
         if(GetComponent<PlayerController>())
         {
-        isPlayer = true;
+            isPlayer = true;
         }
-
     }
-    // can we shooot a bullet
+    // Can we shoot a bullet
     public bool CanShoot()
     {
         if(Time.time - lastShootTime >= shootRate)
         {
-            if(curAmmo > 0 || infiniteAmmo == true)
+            if(curAmmo > 0 || infiniteAmmo == true )
             {
                 return true;
             }
@@ -42,23 +44,11 @@ public class Weapon : MonoBehaviour
     {
         // Adjust shoot time and reduce ammo by one
         lastShootTime = Time.time;
-        curAmmo --;
-
-        // Create projectile
-        GameObject bullet = Instantiate(bulletProjectile, muxzzle.position, muzzle.rotation);
+        curAmmo --; 
+        // Create projectile 
+        GameObject bullet = Instantiate(bulletProjectile, muzzle.position, muzzle.rotation);
 
         // Set Velocity of bulletProjectile
         bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
 }
