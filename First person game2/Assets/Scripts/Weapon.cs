eeef09420ee8d8bfc11d80d670a66ce717471e5a
MIcah Morgan <micah.m239@stu.nebo.edu>
2021-10-27 09:22:08 -0600

    Object pool script

--- First person game2/Assets/Scripts/Weapon.cs
+++ First person game2/Assets/Scripts/Weapon.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject bulletProjectile;
+    public ObjectPool bulletPool;
     public Transform muzzle;
 
     public int curAmmo;
@@ -45,8 +45,11 @@ public class Weapon : MonoBehaviour
         // Adjust shoot time and reduce ammo by one
         lastShootTime = Time.time;
         curAmmo --; 
-        // Create projectile 
-        GameObject bullet = Instantiate(bulletProjectile, muzzle.position, muzzle.rotation);
+        
+        
+        GameObject bullet = bulletPool.GetObject();
+        bullet.transform.position = muzzle.position;
+        bullet.transform.rotation = muzzle.rotation;
 
         // Set Velocity of bulletProjectile
         bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed;
